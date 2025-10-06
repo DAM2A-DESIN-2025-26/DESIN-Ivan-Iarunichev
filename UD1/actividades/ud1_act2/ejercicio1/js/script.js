5bb38cfd943975a88ba90c75df1bf336a3f8585e
Iván Iarunichev <ivani94@educastur.es>
2025-10-06 20:00:02 +0200

    UD1 Act2 Ejercicio2 - Modificado CASA

--- UD1/actividades/ud1_act2/ejercicio1/js/script.js
+++ UD1/actividades/ud1_act2/ejercicio1/js/script.js
@@ -2,28 +2,28 @@ let nombre = prompt("Introduce tu nombre: ");
 let apellido1 = prompt("Introduce tu apellido: ");
 let apellido2 = prompt("Introduce tu segundo apellido: ");
 
-// Número de caracteres sin espacios
+//numero de caracteres sin espacios
 let numCaracteres = nombre.length + apellido1.length + apellido2.length;
 alert(`El número de caractéres sin separación es: ${numCaracteres}`);
 
-// La cadeana en mayúsculas y minúsculas
+//la cadeana en mayúsculas y minúsculas
 alert(`Cadena en mayusculas: ${nombre.toUpperCase()} ${apellido1.toUpperCase()} ${apellido2.toUpperCase()}`);
 alert(`Cadena en minusculas: ${nombre.toLowerCase()} ${apellido1.toLowerCase()} ${apellido2.toLowerCase()}`);
 
-// Que divida el nombre y los apellidos y los muestre en 3 lineas
+//que divida el nombre y los apellidos y los muestre en 3 lineas
 alert(`Nombre: ${nombre} \n Apellido 1: ${apellido1} \n Apellido 2: ${apellido2}`);
 
-// Hacer que se proponga un nombre de usuario
+//hacer que se proponga un nombre de usuario
 let nombreMinuscula = nombre.toLowerCase();
 let apellido1Minuscula = apellido1.toLowerCase();
 let apellido2Minuscula = apellido2.toLowerCase();
 
 alert(`El usuario recomendad: ${nombreMinuscula}${apellido1Minuscula.charAt(0)}${apellido2Minuscula.charAt(0)}`);
 
-// Hacer que se proponga un nombre de usuario diferente
+//hacer que se proponga un nombre de usuario diferente
 alert(`Otra usuario recomendado: ${nombreMinuscula.slice(0,2)}${apellido1Minuscula.slice(0,2)}${apellido2Minuscula.slice(0,2)}`);
 
-// Contraseña separando cada letra por un numero mediante una función
+//contraseña separando cada letra por un numero mediante una función
 function creaPassword(nombre) {
     let password = "";
     for (let i = 0; i < nombre.length; i++) {
