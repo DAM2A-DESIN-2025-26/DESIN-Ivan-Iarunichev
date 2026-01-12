f298d08c0e364516a810bd7eb6df751d9580429d
Ivan Iarunichev <ivani94@educastur.es>
2026-01-12 13:30:02 +0100

    UD2 Actividad 2 - SmartTrackNavbar CASA

--- UD2/actividades/ud2_act2/smarttrackapp/src/app/app.routes.ts
+++ UD2/actividades/ud2_act2/smarttrackapp/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'usuario',
+    loadComponent: () => import('./pages/usuario/usuario').then(m => m.Usuario),
+  },
+
+  {
+    path: 'empresa',
+    loadComponent: () => import('./pages/empresa/empresa').then(m => m.Empresa),
+  },
+
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'usuario',
+  },
+];
---
+++ UD2/actividades/ud2_act2/smarttrackapp/src/app/component/smart-track-navbar/smart-track-navbar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-smart-track-navbar',
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './smart-track-navbar.html',
+  styleUrl: './smart-track-navbar.css',
+})
+export class SmartTrackNavbar {
+}
