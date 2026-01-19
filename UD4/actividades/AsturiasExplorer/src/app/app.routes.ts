c0552df830b4017a6b663a4b2b0d5559c54aa91b
Ivan Iarunichev <ivani94@educastur.es>
2026-01-19 13:35:36 +0100

    UD4 Actividad 1 - Configuracion CASA

--- UD4/actividades/AsturiasExplorer/src/app/app.routes.ts
+++ UD4/actividades/AsturiasExplorer/src/app/app.routes.ts
@@ -17,5 +17,17 @@ export const routes: Routes = [
     path: '',
     redirectTo: 'inicio',
     pathMatch: 'full',
+  },  {
+    path: 'inicio',
+    loadComponent: () => import('./pages/inicio/inicio.page').then( m => m.InicioPage)
   },
+  {
+    path: 'rutas',
+    loadComponent: () => import('./pages/rutas/rutas.page').then( m => m.RutasPage)
+  },
+  {
+    path: 'gastro',
+    loadComponent: () => import('./pages/gastro/gastro.page').then( m => m.GastroPage)
+  },
+
 ];
